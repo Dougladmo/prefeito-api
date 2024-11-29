2d329e016e3c85c79ba9b3fbab74ea3e016ca247
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2024-11-29 14:51:09 -0300

    commit remove admuser

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const admUserController = require("../controllers/admUserController");
 const checkToken = require("../middlewares/authMiddleware");
 
 // Rotas de autenticação para usuários comuns
@@ -12,23 +11,10 @@ router.post("/reset-password", userController.resetPassword); // Redefine a senh
 router.post("/verify-email/user", userController.verifyEmail); // Verifica o email com o código
 router.post("/resend-verification-email", userController.resendVerificationEmail);
 
-// Rotas de autenticação para administradores
-router.post("/adm/register", admUserController.register);
-router.post("/adm/login", admUserController.login);
-router.post("/adm/forgot-password", admUserController.forgotPassword); // Envia o código de 6 dígitos
-router.post("/adm/reset-password", admUserController.resetPassword); // Redefine a senha com o código de 6 dígitos
-router.post("/adm/verify-email", admUserController.verifyEmail); // Verifica o email com o código
-
-// Rota para reenvio de e-mail de verificação para administradores
-router.post("/adm/resend-verification-email", admUserController.resendVerificationEmail);
-
 // Rota para reenvio de e-mail de redefinição de senha para administradores
 router.post("/adm/resend-reset-password-email", admUserController.resendResetPasswordEmail);
 
 // Rota privada para buscar usuário
 router.get("/users", checkToken, userController.getUser);
-
-// Rota privada para buscar administrador
-router.get("/adm/users/:id", checkToken, admUserController.getUser);
 
 module.exports = router;
