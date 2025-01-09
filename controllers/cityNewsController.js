4e1552a551c688d8a0e1549abd84ad7d124a0d7a
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2025-01-09 14:36:05 -0300

    Update cityNewsController.js

--- controllers/cityNewsController.js
+++ controllers/cityNewsController.js
@@ -2,14 +2,24 @@ const { dynamoDB } = require("../config/db");
 const TABLE_NAME = "cityNews";
 
 exports.getAllNews = async (req, res) => {
-    try {
-      const params = {
-        TableName: TABLE_NAME,
-      };
-  
-      const result = await dynamoDB.scan(params);
-      res.status(200).json(result.Items);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
+  try {
+    const { lastEvaluatedKey } = req.query;
+
+    const params = {
+      TableName: TABLE_NAME,
+      Limit: 5,
+      ExclusiveStartKey: lastEvaluatedKey ? JSON.parse(lastEvaluatedKey) : undefined,
+    };
+
+    const result = await dynamoDB.scan(params);
+    res.status(200).json({
+      items: result.Items,
+      lastEvaluatedKey: result.LastEvaluatedKey ? JSON.stringify(result.LastEvaluatedKey) : null,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
+
+
+// os items sao retornados de 5 em 5
