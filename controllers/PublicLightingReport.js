5c2ec1499d4efb522e24a4f375b6e54792d26217
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2025-01-30 18:04:18 -0300

    commit

--- controllers/PublicLightingReport.js
+++ controllers/PublicLightingReport.js
@@ -28,46 +28,94 @@ async function sendEmailWithSES(report) {
       Body: {
         Html: {
           Data: `
-             <!DOCTYPE html>
-            <html lang="pt-BR">
-            <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <title>Detalhes do Relatório do aplicativo</title>
-            </head>
-            <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 20px;">
-              <div style="max-width: 600px; margin: auto; background: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
-                <img src="https://i.imgur.com/fNlG0zM.png" alt="logo c2a" style="width: 120px; margin-bottom: 20px;style="width: 120px; margin-bottom: 20px; background-color: #fff;"">
-                
-                <h1 style="color: #333; font-size: 24px; text-align: center;">Detalhes do Relatório de Trânsito</h1>
-                
-                <div style="margin-top: 20px; border-top: 2px solid #007bff; padding-top: 20px;">
-                  <p style="font-size: 16px; color: #555;">ID do Relatório: <strong>${report._id}</strong></p>
-                  <p style="font-size: 16px; color: #555;">Tipo: <strong>${report.type}</strong></p>
-                  <p style="font-size: 16px; color: #555;">Descrição: <strong>${report.description}</strong></p>
-                  <p style="font-size: 16px; color: #555;">Localização: <strong>${report.location}</strong></p>
-                  <p style="font-size: 16px; color: #555;">Status: <strong>${report.statusReport}</strong></p>
-                  <p style="font-size: 16px; color: #555;">Data: <strong>${new Date(report.createdAt).toLocaleString('pt-BR', {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: 'numeric',
-                    hour: '2-digit',
-                    minute: '2-digit',
-                  }).replace(',', ' ')}</strong></p>
-                </div>
-                
-                <div style="margin-top: 20px; border-top: 1px solid #ddd; padding-top: 20px;">
-                  <h2 style="font-size: 20px; color: #007bff;">Imagem do Relatório</h2>
-                  <img src="${report.image}" alt="Imagem do Relatório" style="width: 100%; max-height: 400px; object-fit: cover; border-radius: 8px; margin-top: 10px;">
-                </div>
-        
-                <div style="margin-top: 30px; font-size: 14px; color: #555;">
-                  <p style="text-align: center;">Se você tiver problemas com o seu relatório ou precisar de mais informações, entre em contato conosco.</p>
-                  <p style="text-align: center;">Atenciosamente,<br>Equipe de Suporte <br> suporte@c2a.com.br </p>
-                </div>
-              </div>
-            </body>
-            </html>
+                <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Detalhes do Relatório do Aplicativo</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f4f4f4;
+          margin: 0;
+          padding: 20px;
+        }
+        .container {
+          max-width: 600px;
+          margin: auto;
+          background: #fff;
+          padding: 20px;
+          border-radius: 8px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        .header img {
+          width: 120px;
+          margin-bottom: 20px;
+          background-color: #fff;
+        }
+        h1 {
+          color: #333;
+          font-size: 24px;
+          text-align: center;
+        }
+        .content p {
+          font-size: 16px;
+          color: #555;
+        }
+        .footer p {
+          text-align: center;
+          font-size: 14px;
+          color: #555;
+        }
+        .img-report {
+          width: 100%;
+          max-height: 400px;
+          object-fit: cover;
+          border-radius: 8px;
+          margin-top: 10px;
+        }
+        .unsubscribe {
+          font-size: 14px;
+          text-align: center;
+          margin-top: 20px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <div class="header">
+          <img src="https://i.imgur.com/fNlG0zM.png" alt="logo c2a">
+        </div>
+        <h1>Detalhes do Relatório de Trânsito</h1>
+        <div class="content">
+          <p><strong>ID do Relatório:</strong> ${report._id}</p>
+          <p><strong>Tipo:</strong> ${report.type}</p>
+          <p><strong>Descrição:</strong> ${report.description}</p>
+          <p><strong>Localização:</strong> ${report.location}</p>
+          <p><strong>Status:</strong> ${report.statusReport}</p>
+          <p><strong>Data:</strong> ${new Date(report.createdAt).toLocaleString('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+              }).replace(',', ' ')}</p>
+        </div>
+        <div class="content">
+          <h2 style="font-size: 20px; color: #007bff;">Imagem do Relatório</h2>
+          <img src="${report.image}" alt="Imagem do Relatório" class="img-report">
+        </div>
+        <div class="footer">
+          <p>Se você tiver problemas com o seu relatório ou precisar de mais informações, entre em contato conosco.</p>
+          <p>Atenciosamente,<br>Equipe de Suporte<br>suporte@c2a.com.br</p>
+        </div>
+        <div class="unsubscribe">
+          <p>Se você deseja se desinscrever da nossa lista, <a href="https://www.seusite.com/cancelar-inscricao">clique aqui</a>.</p>
+        </div>
+      </div>
+    </body>
+    </html>
           `,
         },
       },
